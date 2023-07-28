7c185a16447c5639fecfbfdd6d93768f53b6e1f3
alyffer henrique <alyffer.sprisigo@sistemainfo.com.br>
2023-07-28 17:02:23 -0300

    Ajustando o cadastro da senha

--- app/src/main/java/com/example/testemvvm/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/testemvvm/ui/RegisterActivity.kt
@@ -2,7 +2,6 @@ package com.example.testemvvm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.testemvvm.R
 import com.example.testemvvm.data.models.RegisterModel
 import com.example.testemvvm.data.repositories.RegisterRepository
 import com.example.testemvvm.databinding.ActivityRegisterBinding
@@ -22,26 +21,44 @@ class RegisterActivity : AppCompatActivity() {
 
         viewModel = RegisterViewModel(RegisterRepository())
 
-        binding.usernamePhone.addTextChangedListener(MaskUtil.insert(MaskUtil.CELLPHONE,binding.usernamePhone))
+        binding.usernamePhone.addTextChangedListener(
+            MaskUtil.insert(
+                MaskUtil.CELLPHONE,
+                binding.usernamePhone
+            )
+        )
+
+        binding.usernameDocumentRegister.addTextChangedListener(
+            MaskUtil.insert(
+                MaskUtil.CPF,
+                binding.usernameDocumentRegister
+            )
+        )
 
         binding.ButtonCadastar.setOnClickListener {
             val request = RegisterModel.Request(
-               document =  binding.usernameDocument.text.toString(),
-               name = binding.usernameName.text.toString(),
-               email = binding.usernameEmail.text.toString(),
-               phone =  binding.usernamePhone.text.toString(),
-               birthday = binding.usernameBirthdate.text.toString()
+
+                document = binding.usernameDocumentRegister.text.toString(),
+                name = binding.usernameName.text.toString(),
+                email = binding.usernameEmail.text.toString(),
+                phone = binding.usernamePhone.text.toString(),
+                birthday = binding.usernameBirthdate.text.toString(),
+                password = binding.editPassword.text.toString()
+
+
             )
             if (request.document.isNullOrEmpty()) {
-                this@RegisterActivity.showToastLong("INforme o documento")
+                this@RegisterActivity.showToastLong("Informe o documento")
             } else if (request.email.isNullOrEmpty()) {
-                this@RegisterActivity.showToastLong("INforme o email")
+                this@RegisterActivity.showToastLong("Informe o email")
             } else if (request.name.isNullOrEmpty()) {
-                this@RegisterActivity.showToastLong("INforme o nome")
+                this@RegisterActivity.showToastLong("Imforme o nome")
+            } else if (request.password.isNullOrEmpty()) {
+                this@RegisterActivity.showToastLong("Informe um senha")
             } else if (request.phone.isNullOrEmpty()) {
-                this@RegisterActivity.showToastLong("INforme o telefone")
+                this@RegisterActivity.showToastLong("Informe o telefone")
             } else if (request.birthday.isNullOrEmpty()) {
-                this@RegisterActivity.showToastLong("INforme a data de nascimento")
+                this@RegisterActivity.showToastLong("Informe a data de nascimento")
             } else {
                 viewModel.register(request)
             }
